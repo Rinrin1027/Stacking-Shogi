8615853a1866ecd010249c1c08707454066f7591
Kamiyama <kkimh256@icloud.com>
2024-09-14 17:42:00 +0900

    pull

--- Stacking Shogi/Assets/Scripts/records_logic.cs
+++ Stacking Shogi/Assets/Scripts/records_logic.cs
@@ -13,39 +13,52 @@ public class records_logic : MonoBehaviour
     void Start(string txt)
     {
         createBoard();
-        StreamReader lines = new StreamReader("01.txt"); //txtに置き換える
+        // StreamReader lines = new StreamReader("01.txt"); //txtに置き換える
 
-        foreach (string line in lines)
-        {
-            string[] element = line.Split(',');
+        // foreach (string line in lines)
+        // {
+        //     string[] element = line.Split(',');
 
-            displayText.text = element[0];
-            if(element[4] == 1){
-                string piece = element[5];
-                if (takenPieceCount.ContainsKey(piece))
-                {
-                    takenPieceCount[piece]++;
-                }
-                else
-                {
-                    takenPieceCount.Add(piece, 1);
-                    createNewPieceSlot(piece);
-                }
+        //     displayText.text = element[0];
+        //     if(element[4] == 1){
+        //         string piece = element[5];
+        //         if (takenPieceCount.ContainsKey(piece))
+        //         {
+        //             takenPieceCount[piece]++;
+        //         }
+        //         else
+        //         {
+        //             takenPieceCount.Add(piece, 1);
+        //             createNewPieceSlot(piece);
+        //         }
 
                 
+        //     }
+        // }
+    }
+
+    void createBoard()
+    {
+        boardWidth = 9;
+        boardHeight = 9;
+
+        for(int i = 0; i < boardWidth; i++)
+        {
+            for(int j = 0; j < boardHeight; j++)
+            {
+                GameObject newSlot = new GameObject();
+                newSlot.transform.SetParent(displayText.transform.parent);
+                newSlot.AddComponent<Image>().sprite = Resources.Load<Sprite>("Images/Board.png");
+                newSlot.GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
+                newSlot.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+                newSlot.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+                newSlot.GetComponent<Image>().enabled = true;
             }
         }
     }
 
     void createNewPieceSlot(string piece)
     {
-        GameObject newSlot = new GameObject();
-        newSlot.transform.SetParent(takenPieceImage.transform.parent);
-        newSlot.AddComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/" + piece + ".png");
-        newSlot.GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
-        newSlot.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-        newSlot.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
-        newSlot.GetComponent<Image>().enabled = true;
     }
 
     // Update is called once per frame
