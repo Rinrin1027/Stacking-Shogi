85c1f49cba46199bb79e84645f9d9ae0f4350e08
daidai411411 <daidai411411@gmail.com>
2024-09-16 09:03:45 +0900

    RecordManager作成

---
+++ Stacking Shogi/Assets/Scripts/RecordManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordManager : MonoBehaviour
+{
+    public ShogiBoard shogiBoard; // 将棋盤の管理クラス
+    public ShogiPieceController shogiPieceController; // 駒の配置を行うスクリプト
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
