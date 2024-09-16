c8976fa0ef2e6a1572032bed3b9550a43943339f
daidai411411 <daidai411411@gmail.com>
2024-09-16 09:33:42 +0900

    棋譜画面途中

--- Stacking Shogi/Assets/Scripts/RecordManager.cs
+++ Stacking Shogi/Assets/Scripts/RecordManager.cs
@@ -6,6 +6,7 @@ public class RecordManager : MonoBehaviour
 {
     public ShogiBoard shogiBoard; // 将棋盤の管理クラス
     public ShogiPieceController shogiPieceController; // 駒の配置を行うスクリプト
+    public int recordIndex = 0; // 現在の手数
     
     
     // Start is called before the first frame update
@@ -18,5 +19,23 @@ public class RecordManager : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    void OnPrevButtonPressed()
+    {
+        if (recordIndex > 0)
+        {
+            recordIndex--;
+            shogiPieceController.SetPieces(shogiBoard.records[recordIndex]);
+        }
+    }
+    
+    void OnNextButtonPressed()
+    {
+        if (recordIndex < shogiBoard.records.Count - 1)
+        {
+            recordIndex++;
+            shogiPieceController.SetPieces(shogiBoard.records[recordIndex]);
+        }
     }
 }
